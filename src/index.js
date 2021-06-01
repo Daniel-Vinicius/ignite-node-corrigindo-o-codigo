8a1ca9bb68816cedfe5c68c27d67bfd416f7d0fd
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-06-01 12:02:26 -0300

    🎉 Desafio Finalizado!

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body
 
+  if (!title || !url || !techs) {
+    return response.status(400).json({ error: 'Filds title or url or techs missing' });
+  }
+
   const repository = {
     id: uuid(),
     title,
@@ -23,20 +27,25 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repo = { ...repositories[repositoryIndex] };
+
+  const repository = { ...repositories[repositoryIndex], ...updatedRepository, likes: repo.likes };
 
   repositories[repositoryIndex] = repository;
 
@@ -48,7 +57,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -68,7 +77,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
